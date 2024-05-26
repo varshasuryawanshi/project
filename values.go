89893c03606728a03bbee381868cf6308a110557
Varsha Suryawanshi <suryawanshivarsha14@gmail.com>
2024-05-26 13:35:23 +0530

    Add values

---
+++ values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a:=1+1
+	fmt.Println(a)
+	fmt.Println("varsha " + "suryawanshi")
+
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("2*2 = ", 2*2)
+	fmt.Println("4/2 = ", 4/2)
+	
+	fmt.Println(true && true)
+	fmt.Println(false || false)
+}
+
