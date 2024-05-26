459ffca25726f522eea2904d9b7da2b3ee8e79bb
Varsha Suryawanshi <suryawanshivarsha14@gmail.com>
2024-05-26 14:03:13 +0530

    Add variable in go

---
+++ variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Hello Varsha"
+	fmt.Println(a)
+	
+	var b = 2 
+	var c = 3
+	fmt.Println(b , c)
+	
+	var d, e float32 = 2.4 , 3.2
+	fmt.Println(d, e)
+	
+	var f = true
+	fmt.Println(f)
+
+	var g int
+	fmt.Println(g)
+
+	h := "apple"
+	fmt.Println(h)
+}
